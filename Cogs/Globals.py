083b3ca1947ddb04e001088c63c1ebed412a232c
Ex0tic-Python <80157573+Ex0tic-Python@users.noreply.github.com>
2021-07-13 14:00:04 -0500

    Updated Globals.py file
    Fixed it up a bit
    Made it look better, added some stuff, and thats about it

--- Cogs/Globals.py
+++ Cogs/Globals.py
@@ -1,40 +1,48 @@
-# Functions
+# Global Stuff
+# Basically a Python file that stores stuff (Vars, Funcs, etc.) that I may need in one or more Python files
+# Also stores vars that may change constantly, mking it easier for me to access them and change them
+
 # ---------------------------------------------------------------------------------------------------------
+
 # Imports
-# OS for joining paths
-import os
+# Provides us with a way to work with paths (Used in the bot_folder_path function)
+from os import path
 
 # ---------------------------------------------------------------------------------------------------------
 
 # Variables
-# A variable which stores the number of lines of the script
-script_lines = 999
-
-# Bot version
-bot_version = 999
-
-# Total commands of the bot
-total_commands = 999
-
-# Link to update notes
-update_notes = 'https://pastebin.com/zBtqhaaT'
-
 # Path to bots image
 bot_pfp = r'C:\Users\AnibalF6725\Documents\Code\Python\DiscordBot\Ex0tic_Bot.jpg'
 
+# Bot version (Used in extra_info command)
+bot_version = 0.5
+
+# Total commands of the bot (Used in extra_info command)
+total_commands = 999
+
+# A variable which stores the number of lines of the script (Used in extra_info command)
+script_lines = 999
+
+# Link to update notes (Used in extra_info command)
+update_notes = 'https://github.com/Ex0tic-Python/Ex0tic_Bot/blob/main/UPDATE%20NOTES.md'
+
+# List containing the ID's of the bot dev team (Used in commands which require special access) (Currently only me :/ )
+dev_ids = (546390125425459211)
+
 # Dictionary that houses emojis the bot uses
 emojis = {
-    'discord_typing':'<a:discord_typing:864533501189423144>',
+    'exotic_bot':'<:Ex0tic_Bot:864541148647391232>',
     'discord_online':'<:discord_online:864533505534197821>',
     'discord_offline':'<:discord_offline:864533501066608660>',
-    'discord_ex0tic_bot':'<:Ex0tic_Bot:864541148647391232>',
+    'discord_typing':'<a:discord_typing:864533501189423144>',
     'discord_message':'<:discord_message:864548909152731147>',
-    'discord_db':'<:discord_db:864548909199392798>'
+    'database':'<:database:864548909199392798>',
+    'spinning_cog':'<a:spinning_cog:864580163021963294>'
 }
 
 # ---------------------------------------------------------------------------------------------------------
 
 # Functions
-# Funtion that returns the path to a certain File in the Bot Folder
-def bot_folder_path(file_to_join: str) -> str:
-    return os.path.join(r'C:\Users\AnibalF6725\Documents\Code\Python\DiscordBot', file_to_join)
+# Funtion that returns a joined path
+def bot_folder_path(path_to_join: str) -> str:
+    return os.path.join(r'C:\Users\AnibalF6725\Documents\Code\Python\DiscordBot', path_to_join)
